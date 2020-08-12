2bf4f75653d2a5917664d54efcc17010c14bbecf
Adam Miloch <miloch.adam@gmail.com>
2020-08-12 20:45:55 +0200

    Added verbose

--- make_links.sh
+++ make_links.sh
@@ -4,12 +4,12 @@
 MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 
-ln -sf $MY_PATH/vim/.vimrc ~
+ln -sfv $MY_PATH/vim/.vimrc ~
 mkdir ~/.vim/
-ln -sf $MY_PATH/vim/.config ~/.vim/
-ln -sf $MY_PATH/vim/UltiSnips ~/.vim/
+ln -sfv $MY_PATH/vim/.config ~/.vim/
+ln -sfv $MY_PATH/vim/UltiSnips ~/.vim/
 
-ln -sf $MY_PATH/tmux/.tmux.conf ~
+ln -sfv $MY_PATH/tmux/.tmux.conf ~
 
-ln -sf $MY_PATH/zsh/.zshrc ~
-ln -sf $MY_PATH/bash/.bash_aliases ~
+ln -sfv $MY_PATH/zsh/.zshrc ~
+ln -sfv $MY_PATH/bash/.bash_aliases ~
