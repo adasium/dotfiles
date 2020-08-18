a80b05a2c0ddb0ae206492c75b74c4e5b8734d56
Adam Miloch <miloch.adam@gmail.com>
2020-08-18 14:23:43 +0200

    Added emacsclient.desktop entry

--- spacemacs/setup.sh
+++ spacemacs/setup.sh
@@ -5,3 +5,4 @@ SCRIPT_DIR=$(dirname $(realpath -s $0))
 ln -sfv $SCRIPT_DIR/.spacemacs ~/
 ln -sfv $SCRIPT_DIR/snippets/* ~/.emacs.d/private/snippets/
 ln -sfv $SCRIPT_DIR/emacs.service ~/.config/systemd/user/
+ln -sfv $SCRIPT_DIR/emacsclient.desktop ~/.local/share/applications/
