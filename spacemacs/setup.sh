0ae944f893a190f69e9f7eb3f63e423637ca1414
Adam Miloch <miloch.adam@gmail.com>
2020-09-02 22:38:44 +0200

    Make sure directories exist

--- spacemacs/setup.sh
+++ spacemacs/setup.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR=$(dirname $(realpath -s $0))
 
+mkdir -vp ~/.config/emacs/
+mkdir -vp ~/.config/systemd/user/
+
 ln -sfv $SCRIPT_DIR/.spacemacs ~/
 ln -sfv $SCRIPT_DIR/snippets/* ~/.emacs.d/private/snippets/
 ln -sfv $SCRIPT_DIR/emacs.service ~/.config/systemd/user/
