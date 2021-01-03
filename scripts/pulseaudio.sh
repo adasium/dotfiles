ac2efd649b5015272272edbc26cd0cbf13da0557
milochadam <miloch.adam@gmail.com>
2021-01-03 23:27:41 +0100

    Added info flag

--- scripts/pulseaudio.sh
+++ scripts/pulseaudio.sh
@@ -5,6 +5,7 @@ GET_DEFAULT_CMD="tr '\n' '|' | sed -e 's/*\{0,1\}[[:space:]]*index/\n\0/g' | gre
 DEFAULT_INPUT_CMD="pacmd list-sources | $GET_DEFAULT_CMD | sed 's/|/\n/g' | grep 'name:' | awk -F '<|>' '{print \$2}'"
 DEFAULT_OUTPUT_CMD="pacmd list-sinks | $GET_DEFAULT_CMD | sed 's/|/\n/g' | grep 'name:' | awk -F '<|>' '{print \$2}'"
 
+LATENCY_MSEC=5
 
 MICROPHONE=$(eval "$DEFAULT_INPUT_CMD")
 SPEAKERS=$(eval "$DEFAULT_OUTPUT_CMD")
@@ -14,7 +15,12 @@ VIRTUAL2_EXISTS="$(pactl list sinks short | grep virtual2)"
 
 echo "Default input:  $MICROPHONE"
 echo "Default output: $SPEAKERS"
-echo ""
+
+if [ "$1" == "--info" ] || [ "$1" == "-i" ]; then
+    exit 0
+else
+    echo ""
+fi
 
 if [ "$1" == "--teardown" ] || [ "$1" == "-t" ]; then
     echo "Unloading modules containing string 'virtual'"
@@ -38,11 +44,11 @@ if [ ! "$VIRTUAL1_EXISTS" ] && [ ! "$VIRTUAL2_EXISTS" ]; then
     pactl load-module module-null-sink sink_name=virtual1 sink_properties=device.description="virtual1"
     pactl load-module module-null-sink sink_name=virtual2 sink_properties=device.description="virtual2"
 
-    pactl load-module module-loopback source=virtual1.monitor sink=$SPEAKERS latency_msec=5
-    pactl load-module module-loopback source=virtual1.monitor sink=virtual2 latency_msec=5
-    pactl load-module module-loopback source=$MICROPHONE sink=virtual2 latency_msec=5
-
-    # Applications like Discord will probably not be able to set monitor device as input.
-    # We workaround it by setting virtual2.monitor as default input which makes it default in the app.
+    pactl load-module module-loopback source=virtual1.monitor sink=$SPEAKERS latency_msec=$LATENCY_MSEC
+    pactl load-module module-loopback source=virtual1.monitor sink=virtual2 latency_msec=$LATENCY_MSEC
+    pactl load-module module-loopback source=$MICROPHONE sink=virtual2 latency_msec=$LATENCY_MSEC
 fi
+
+# Applications like Discord will probably not be able to set monitor device as input.
+# We workaround it by setting virtual2.monitor as default input which makes it default in the app.
 pacmd set-default-source virtual2.monitor
