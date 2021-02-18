e9f7fd5d79d82a73a3f80dc46675a4ec03630c7f
milochadam <miloch.adam@gmail.com>
2021-02-19 00:24:27 +0100

    pulseaudio.sh script upgrade?

--- scripts/pulseaudio.sh
+++ scripts/pulseaudio.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 # https://github.com/toadjaune/pulseaudio-config
 
-GET_DEFAULT_CMD="tr '\n' '|' | sed -e 's/*\{0,1\}[[:space:]]*index/\n\0/g' | grep '* index'"
-DEFAULT_INPUT_CMD="pacmd list-sources | $GET_DEFAULT_CMD | sed 's/|/\n/g' | grep 'name:' | awk -F '<|>' '{print \$2}'"
-DEFAULT_OUTPUT_CMD="pacmd list-sinks | $GET_DEFAULT_CMD | sed 's/|/\n/g' | grep 'name:' | awk -F '<|>' '{print \$2}'"
+JOIN_EVERYTHING_WITH="tr '\n'"
+ADD_NEWLINE_FOR_EACH_DEVICE="sed -e 's/*\{0,1\}[[:space:]]*index/\n\0/g'"
+UNJOIN_BY_PIPE="sed 's/|/\n/g'"
+GREP_DEFAULT="grep '* index'"
+GET_NAME="grep 'name:' | awk -F '<|>' '{print \$2}'"
+GET_DEFAULT="$JOIN_EVERYTHING_WITH '|' | $ADD_NEWLINE_FOR_EACH_DEVICE | $GREP_DEFAULT | $UNJOIN_BY_PIPE | $GET_NAME"
+
+DEFAULT_INPUT_CMD="pacmd list-sources | $GET_DEFAULT"
+DEFAULT_OUTPUT_CMD="pacmd list-sinks | $GET_DEFAULT"
 
 LATENCY_MSEC=5
 
-MICROPHONE=$(eval "$DEFAULT_INPUT_CMD")
-SPEAKERS=$(eval "$DEFAULT_OUTPUT_CMD")
+DEFAULT_MICROPHONE=$(eval "$DEFAULT_INPUT_CMD")
+DEFAULT_SPEAKERS=$(eval "$DEFAULT_OUTPUT_CMD")
 
-VIRTUAL1_EXISTS="$(pactl list sinks short | grep virtual1)"
-VIRTUAL2_EXISTS="$(pactl list sinks short | grep virtual2)"
+SCRIPT_ID="audio_sharing_script"
 
-echo "Default input:  $MICROPHONE"
-echo "Default output: $SPEAKERS"
+echo "Default input:  $DEFAULT_MICROPHONE"
+echo "Default output: $DEFAULT_SPEAKERS"
 
 if [ "$1" == "--info" ] || [ "$1" == "-i" ]; then
     exit 0
@@ -22,33 +27,38 @@ else
     echo ""
 fi
 
+
+SHARED_SINK_NAME="SHARED_$SCRIPT_ID"  # for both speakers and microphone
+MIC_SINK_NAME="MIC_ONLY_$SCRIPT_ID"   # mic device and SHARED_SINK will be redirected to this sink
+                                      # you can also play audio into this sink without hearing it
+
+SHARED_SINK_EXISTS="$(pactl list sinks short | grep $SHARED_SINK_NAME)"
+MIC_SINK_EXISTS="$(pactl list sinks short | grep $MIC_SINK_NAME)"
+
+
 if [ "$1" == "--teardown" ] || [ "$1" == "-t" ]; then
-    echo "Unloading modules containing string 'virtual'"
-    pactl list modules short | awk '/virtual/{print $1}' | xargs -t -I{} pactl unload-module {}
+    echo "Unloading modules"
+    pactl list modules short | awk "/$SCRIPT_ID/"'{print $1}' | tac | xargs -t -I{} pactl unload-module {}
     exit 0
 fi
 
-
-if [ "$VIRTUAL1_EXISTS" ] || [ "$VIRTUAL2_EXISTS" ]; then
-    # pactl move-sink-input
-    MODULE1="pactl list modules short | awk ' /sink=virtual2/ && !/source=virtual1.monitor/ {print \$1}'"
-    MODULE2="pactl list modules short | awk '!/sink=virtual2/ &&  /source=virtual1.monitor/ {print \$1}'"
-    echo "Unloading module: $(eval $MODULE2)"
-    pactl unload-module $(eval "$MODULE2")
-
-    pactl load-module module-loopback source=virtual1.monitor sink=$SPEAKERS latency_msec=5
+if [ "$SHARED_SINK_EXISTS" ] || [ "$MIC_SINK_EXISTS" ]; then
+    echo "Script modules seem to exist. Did you want to unload them and forgot --teardown flag?"
+    exit 1
 fi
 
-if [ ! "$VIRTUAL1_EXISTS" ] && [ ! "$VIRTUAL2_EXISTS" ]; then
+if [ ! "$SHARED_SINK_EXISTS" ] && [ ! "$MIC_SINK_EXISTS" ]; then
     echo "Creating virtual sinks"
-    pactl load-module module-null-sink sink_name=virtual1 sink_properties=device.description="virtual1"
-    pactl load-module module-null-sink sink_name=virtual2 sink_properties=device.description="virtual2"
+    pactl load-module module-null-sink sink_name=$SHARED_SINK_NAME sink_properties=device.description="$SHARED_SINK_NAME"
+    pactl load-module module-null-sink sink_name=$MIC_SINK_NAME sink_properties=device.description="$MIC_SINK_NAME"
 
-    pactl load-module module-loopback source=virtual1.monitor sink=$SPEAKERS latency_msec=$LATENCY_MSEC
-    pactl load-module module-loopback source=virtual1.monitor sink=virtual2 latency_msec=$LATENCY_MSEC
-    pactl load-module module-loopback source=$MICROPHONE sink=virtual2 latency_msec=$LATENCY_MSEC
+    # splitting audio from shared sink to speakers and mic (mic sink)
+    pactl load-module module-loopback source=$SHARED_SINK_NAME.monitor sink=$DEFAULT_SPEAKERS latency_msec=$LATENCY_MSEC
+    pactl load-module module-loopback source=$SHARED_SINK_NAME.monitor sink=$MIC_SINK_NAME latency_msec=$LATENCY_MSEC
+    # redirect mic to mic sink (which will be set as default)
+    pactl load-module module-loopback source=$DEFAULT_MICROPHONE sink=$MIC_SINK_NAME latency_msec=$LATENCY_MSEC
 fi
 
 # Applications like Discord will probably not be able to set monitor device as input.
 # We workaround it by setting virtual2.monitor as default input which makes it default in the app.
-pacmd set-default-source virtual2.monitor
+pacmd set-default-source $MIC_SINK_NAME.monitor
