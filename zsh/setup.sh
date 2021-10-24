bda48aba8c96a3254918d4c4f7b77894067d07b8
milochadam <miloch.adam@gmail.com>
2021-10-24 04:16:34 +0200

    Fixed zshrc path

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$(dirname $(realpath -s $0))
 COMMIT=57e8c959a02ca8c2aeda3980480a50a1f82f6953
 
-ln -sfv $SCRIPT_DIR/zsh/.zshrc ~
+ln -sfv $SCRIPT_DIR/.zshrc ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/$COMMIT/tools/install.sh)"
