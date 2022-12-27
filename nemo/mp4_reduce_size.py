84f1664a241ece7a952a87bd1b1cb67693ec929b
milochadam <miloch.adam@gmail.com>
2022-12-27 10:24:40 +0100

    add nemo script reduce mp4 size

---
+++ nemo/mp4_reduce_size.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import logging
+import os
+import re
+import subprocess
+from datetime import datetime
+from typing import List
+from typing import Optional
+from typing import Sequence
+
+logging.basicConfig(
+    filename='/tmp/mp4_reduce_size.log',
+    filemode='a',
+    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+    datefmt='%H:%M:%S',
+    level=logging.DEBUG,
+)
+logger = logging.getLogger('mp4_reduce_size.py')
+
+
+def get_filenames() -> List[str]:
+    files = os.environ['NEMO_SCRIPT_SELECTED_FILE_PATHS'].strip().split('\n')
+    _log_files = "\n\t".join(f'"{file}"' for file in files)
+    logger.debug(f'Selected files:\n\t{_log_files}')
+    return files
+
+
+def dirname(filename: str) -> str:
+    """
+    Example:
+    input: /path/to/some/file.txt
+    output: /path/to/some/file
+    """
+    return os.path.splitext(filename)[0]
+
+
+def increment_version(match) -> str:
+    version_number = int(match.group(1))
+    return f'_v{version_number + 1}.mp4'
+
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    version_pattern = r'_v(\d+).mp4'
+    try:
+        for filename in get_filenames():
+            if re.search(version_pattern, filename):
+                out_filename = re.sub(version_pattern, increment_version, filename)
+            else:
+                out_filename = f'{dirname(filename)}_v2.mp4'
+            result = subprocess.check_output(
+                f'/usr/bin/ffmpeg -i {filename} -vcodec libx265 -crf 29 {out_filename}',
+                stderr=subprocess.STDOUT,
+                shell=True,
+            )
+    except Exception as e:
+        logger.exception(e)
+
+    logger.debug(f'Result: {err}')
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
--- nemo/setup.sh
+++ nemo/setup.sh
@@ -4,3 +4,4 @@ SCRIPT_DIR=$(dirname $(realpath -s $0))
 
 mkdir -p ~/.local/share/nemo/scripts/
 ln -sfv $SCRIPT_DIR/pdfunite.py ~/.local/share/nemo/scripts/
+ln -sfv $SCRIPT_DIR/mp4_reduce_size.py ~/.local/share/nemo/scripts/
