64505738bb06a9eabe7e9048af684d9ca09c6a92
milochadam <milochadam@gmail.com>
2023-02-21 14:16:09 +0100

    install vim plugins for neovim also

--- vim/setup.sh
+++ vim/setup.sh
@@ -2,10 +2,22 @@
 
 SCRIPT_DIR=$(dirname $(realpath -s $0))
 
+
+if command -v nvim &> /dev/null
+then
+    DEST_DIR="$HOME/.config/nvim/"
+    VIM=nvim
+    VIMRC_PATH="$DEST_DIR/init.vim"
+else
+    DEST_DIR="$HOME/.vim/"
+    VIM=vim
+    VIMRC_PATH="$HOME/.vimrc"
+fi
+
 echo "Installing Vim-Plug to ~/.vim/autoload/plug.vim"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo $DEST_DIR/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -sfv $SCRIPT_DIR/.vimrc ~/
-ln -sfv $SCRIPT_DIR/.config ~/.vim/
-ln -sfv $SCRIPT_DIR/UltiSnips ~/.vim/
-vim -c PlugInstall +qall
+ln -sfv $SCRIPT_DIR/.vimrc $VIMRC_PATH
+ln -sfv $SCRIPT_DIR/.config $DEST_DIR
+ln -sfv $SCRIPT_DIR/UltiSnips $DEST_DIR
+$VIM -c PlugInstall +qall
