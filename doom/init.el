049f8b0dc566a426486644d9db6e3acedc61e087
milochadam <milochadam@gmail.com>
2023-08-17 19:49:06 +0200

    add lsp ig

--- doom/init.el
+++ doom/init.el
@@ -158,6 +158,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python
+        +lsp
         +cython
         )            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
