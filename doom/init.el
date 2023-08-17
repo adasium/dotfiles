7cfe8cf955d0004cd589f0e67eeb69f69a75801e
milochadam <milochadam@gmail.com>
2023-08-17 20:07:08 +0200

    nvm it's shit

--- doom/init.el
+++ doom/init.el
@@ -158,7 +158,6 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python
-        +lsp
         +cython
         )            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
